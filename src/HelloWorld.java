98a9e23be09042eab4c75313bc91aa8e6f6e8840
ThomaLa <thomas.lapotre@gmail.com>
2017-11-07 16:44:35 +0100

    added «now» to HW greet

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Hello ASE2017 - How are you? :)\nPlease enter your name: ");
+		System.out.print("Hello ASE2017 - How are you now? :)\nPlease enter your name: ");
 		HelloUser user = new HelloUser(sc.next());		
 		System.out.println(user.greetUser());
 		sc.close();
